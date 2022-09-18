94755e08bec9d6f709fa2dd7f6c403b38e1081d8
raaghavgupta2020 <raaghavgupta2020@gmail.com>
2022-09-18 12:10:51 +0530

    basic changes

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//index.js is basically taking the 
+const root = ReactDOM.createRoot(document.getElementById('root'));//this root is basically the id of the file where these compenents are being rendered 
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
