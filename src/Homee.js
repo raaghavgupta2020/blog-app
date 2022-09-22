5a75ffb0454245055743b5d3b5c41fb5fb6cc35f
raaghavgupta2020 <raaghavgupta2020@gmail.com>
2022-09-23 02:12:18 +0530

    errorHandlingStart

--- src/Homee.js
+++ src/Homee.js
@@ -24,18 +24,20 @@ const Homee = () => {
     //     {title: 'Product description' , body: 'lorem ipsum ...', author: 'mario', id: '3'}
     // ])
 
-    const [blogs , setBlogs] = useState([
-        {title: 'My new website ' , body: 'lorem ipsum ...', author: 'mario', id: '1'},
-        {title: 'New Blog' , body: 'lorem ipsum ...', author: 'yoshi', id: '2'},
-        {title: 'Product description' , body: 'lorem ipsum ...', author: 'mario', id: '3'}
-    ])
+    const [blogs , setBlogs] = useState(null)
+    //once we fetch the data using the API endpoint we will update the state using the setBlogs
+    //but initially we are taking it as null
+
+    //creating a conditional loading message 
+    const[isPending , setIsPending]= useState(true)
+
+    const[error , setError] = useState(null)
     
     const handleDelete = (id) => {
         const newBlogs = blogs.filter((blog)=>(
             blog.id!==id
         ))
         setBlogs(newBlogs);
-
     }
 
     //this function will fire on every render
@@ -47,9 +49,36 @@ const Homee = () => {
     //     console.log(name);
     // },[name]);
 
-    useEffect(()=>{
-        console.log("re rendering took place , hence useEffect function ran");
-        console.log(name);
+    useEffect(()=>{ //we cannot make this anonymous func inside useEffect as async
+        // console.log("re rendering took place , hence useEffect function ran");
+        // console.log(name);
+
+        setTimeout(()=>{
+            fetch("http://localhost:8000/blogs") //this returns to us a promise
+            .then((res)=>{//response has an "ok" property
+                //this property will be false in case the data received is faulty
+                if(!res.ok){
+                    //we will throw an error
+                    throw Error('could not fetch the data for that resource')
+                    //we will catch below
+                }
+                return res.json() //this passes the json into a javascript object for us
+            })
+            //the above thing returns another promise as res.json in asynchronous and takes some time to run
+            .then((data)=>{
+                // console.log(data);
+                setBlogs(data)
+                setIsPending(false); //as soon as isPending becomes false Loading... div disappears
+            })
+            .catch((err)=>{ //e here refers to any kind of network error while fetching the data
+                console.log(err.message)
+            })//this will catch any kind of network error
+            //failed to fetch error will be shown if server is unreachable 
+            //now there can be cases where error can be returned by the server , that will come in response
+
+
+
+        }, 2000)//this means 2000ms
     },[]);
 
     //There is something called as a dependency array which can be used to allow function inside useEffect hook to run for 
@@ -86,25 +115,33 @@ const Homee = () => {
             {/* all the above code can be made resuuable by creating a resuable compenent and
             passing blogs arraya and other required data as props */}
 
-            <BlogList blogs={blogs} title={"All blogs"}/>
+            {/* <BlogList blogs={blogs} title={"All blogs"}/> */}
 
             {/* filter basically filters the array an removes the elements from the array which do not
             satisfy the condtions inside . Now a refined array is going in the same BlogList component
             and output is being generated */}
 
-            <BlogList blogs={blogs.filter((blog)=>(
+            {/* <BlogList blogs={blogs.filter((blog)=>(
                 blog.author === 'mario' //checks comparison strictly
-            ))} title={"Mario's blogs"}/>
+            ))} title={"Mario's blogs"}/> */}
 
             {/* here blogs is a property name and {blogs} is the value and this complete thing i.e
             blogs = {blogs} is called as a prop 
             We can pass multiple props also*/}
 
-            <BlogList blogs={blogs} title={"All blogs"} handleDelete={handleDelete}/>
+            {/* <BlogList blogs={blogs} title={"All blogs"} handleDelete={handleDelete}/>
 
             <button onClick={()=>{setName("mithu")}}>Click me to change "name" state </button>
-            <p>{name}</p>
+            <p>{name}</p> */}
+
             {/* here we are changing the state */}
+
+            <div className="fetchingdata">
+                {/* if isPending is true we will show a div */}
+                {isPending && <div>Loading... Fetching data from API</div>}
+                {blogs &&  <BlogList blogs={blogs} title={"Blogs"} handleDelete={handleDelete}/>}
+            </div>
+            {/* as we have put this blogs && and we have covered the thing with a {} , it means it is a conditional statement */}
 
 
         </div>
